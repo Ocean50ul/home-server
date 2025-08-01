c9752c558455e493536133a2f8f932f9edbebf1e
Ocean50ul <iliyankov@gmail.com>
2025-08-01 20:32:45 +0300

    fixed audio_fixtures json and rs, and made prepare to create all the things

--- src/utils/audio_fixtures.rs
+++ src/utils/audio_fixtures.rs
@@ -1,11 +1,11 @@
-use std::{fs::{read_to_string, File}, path::PathBuf};
+use std::{fs::read_to_string, path::PathBuf};
 
 use serde::{Deserialize, Serialize};
 
 use crate::{domain::audiofile::{AudioFileMetadata, AudioFileType}, utils::config::Config};
 
 #[derive(Debug, thiserror::Error)]
-enum FixturesLoadingError {
+pub enum FixturesLoadingError {
     #[error("IO error during tests fixtures loading: {0}")]
     IOError(#[from] std::io::Error),
 
@@ -15,14 +15,18 @@ enum FixturesLoadingError {
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct AudioFixture {
-    file_type: AudioFileType,
-    file_name: PathBuf,
+    pub file_type: AudioFileType,
+    pub file_name: PathBuf,
 
-    metadata: AudioFileMetadata
+    pub metadata: AudioFileMetadata
 }
 
-fn load_fixtures(config: Config) -> Result<Vec<AudioFixture>, FixturesLoadingError> {
-    let json_string = read_to_string(config.media.audio_fixtures_json_path)?;
+impl AudioFixture {
+
+}
+
+pub fn load_fixtures(config: &Config) -> Result<Vec<AudioFixture>, FixturesLoadingError> {
+    let json_string = read_to_string(&config.media.audio_fixtures_json_path)?;
     let fixtures = serde_json::from_str(&json_string)?;
 
     Ok(fixtures)
